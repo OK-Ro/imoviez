f30c57abf6d43a91882e68c990d47528ce4f32ea
Robert <125899158+OK-Ro@users.noreply.github.com>
2023-10-13 00:48:31 +0200

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -8,7 +8,7 @@ function Video() {
         <Header>
           <h2>Romantic</h2>
         </Header>
-        <VideoElement autoPlay muted loop controls={false}>
+        <VideoElement muted loop controls={false}>
           <source src="/Romantic.mp4" type="video/mp4" />
         </VideoElement>
       </VideoContainer>
@@ -17,7 +17,7 @@ function Video() {
         <Header>
           <h2>Action</h2>
         </Header>
-        <VideoElement autoPlay muted loop controls={false}>
+        <VideoElement muted loop controls={false}>
           <source src="/action.mp4" type="video/mp4" />
         </VideoElement>
       </VideoContainer>
@@ -26,7 +26,7 @@ function Video() {
         <Header>
           <h2>Horror</h2>
         </Header>
-        <VideoElement autoPlay muted loop controls={false}>
+        <VideoElement muted loop controls={false}>
           <source src="/horror.mp4" type="video/mp4" />
         </VideoElement>
       </LargeVideoContainer>
